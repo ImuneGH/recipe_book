9acd9c2f497699e2dadb5ceb2e4d20f9d5e3d4fc
ImuneGH <MartinSotnar93@gmail.com>
2025-02-01 15:46:14 +0100

    pridal jsem logo, upravil ho (zatim mam 2 verze), posledne jsem zapomnel commitnout :D ted me ceka tvorba componentu a priblizneho layoutu

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './style.css'
 function App() {
   // komponent Header
   function Header() {
-    return <div className="header pokus"><h1>Nadpis AHOJ</h1></div>
+    return <div className="header pokus"><h1><img src="./img/flavor_log_logo.png" alt="Logo webu Flavor Log" /></h1></div>
   }
   // hlavní aplikace, do které vkládám komponent header
   return (
     <>
       <Header />
-      <div className="pokus"><p>Ahoj</p></div>
+      <div className="pokus"><h2>Ahoj</h2></div>
       <div className="pokus"><p>Ahoj ahoj</p></div>
     </>
   )
