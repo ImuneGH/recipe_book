fa4ce1d2c750691356d9ae31ce8bf045b765f7be
ImuneGH <MartinSotnar93@gmail.com>
2025-02-01 17:34:10 +0100

    obalení aside a content

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Aside from './components/Aside'
 import Footer from './components/Footer'
 
 function App() {
-  // hlavní aplikace, do které vkládám komponent header
   return (
     <div className='app'>
       <Header />
-      <Aside />
-      <Content />
+      <div className="mainContent">
+        <Aside />
+        <Content />
+      </div>
       <Footer />
     </div>
   )
