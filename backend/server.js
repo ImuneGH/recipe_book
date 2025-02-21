68c27113c5fcb5fef86b0f8cc6bfb5ac656702c2
ImuneGH <MartinSotnar93@gmail.com>
2025-02-21 20:32:20 +0100

    pridal jsem databazi, server.js a s tim celou novou backend slozku. podarilo se mi vytvorit tabulku v databazi a zobrazit ji v localhostu, priste se pokusim propojit s Reactem, wish me luck, bude to jizda

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+// něco jako importy
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors()); // propojení backend, frontend
+app.use(express.json()); // Pro práci s JSON daty
+
+// Připojení k databázi
+const db = new sqlite3.Database("./database.db", (err) => {
+  if (err) {
+    console.error("Chyba připojení k DB:", err.message);
+  } else {
+    console.log("✅ Připojeno k databázi SQLite");
+  }
+});
+
+// GET requesty (z databáze přetvoří tabulku recipes v json)
+app.get("/recipesok", (req, res) => {
+  db.all("SELECT * FROM recipes", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// POST requesty (vložení dat do tabulky recipes)
+app.post("/recipes", (req, res) => {
+  const { recipe_name, ingredients, instructions } = req.body;
+  if (!nazev || !popis) {
+    return res.status(400).json({ error: "Vyplňte všechna pole!" });
+  }
+
+  db.run("INSERT INTO recipes (recipe_name, ingredients, instructions) VALUES (?, ?, ?)", [recipe_name, ingredients, instructions], function (err) {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ id: this.lastID, recipe_name, ingredients, instructions });
+  });
+});
+
+// Spuštění serveru
+app.listen(PORT, () => {
+  console.log(`🚀 Server běží na http://localhost:${PORT}`);
+});
