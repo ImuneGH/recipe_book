dc26fb4143aeaf7279f4827ddc6267639e11922a
ImuneGH <MartinSotnar93@gmail.com>
2025-03-23 13:11:30 +0100

    proste jsem potreboval commit

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,7 +2,7 @@ import '../css/recipeCard.css'
 
 const RecipeCard = ({ filteredRecipe }) => {
     return <div className="recipeCard">
-                <img src="" alt="" />
+                <img src={filteredRecipe.img} alt="" />
                 <div className="recipeInfo">
                     <p title='Kategorie'>{filteredRecipe.category}</p>
                     <p title='Doba přípravy'>⏳ {filteredRecipe.cookTime}</p>
