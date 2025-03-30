e589b64e83b29439ace49a9693b5c1fc1d3315c5
ImuneGH <MartinSotnar93@gmail.com>
2025-03-30 16:52:43 +0200

    upravil jsem newRecipeForm, úplně spokojen s designem nejsem, ale kus práce je za mnou, ještě zvažuju pár věcí, jako třeba jak udělat kategorie, protože select znamená, že lze zvolit jen jednu variantu, což ale ne vždy bude pravda, takže možná nakonec zvolím checkboxy nebo něco jiného. Potom asi zkusím natvrdo poslat data do DB a pak bych teprve zkoušel fixovat všemožné blbosti, které by se daly zprznit při zápisu do inputů. (např. příliš dlouhý název, špatný formát ingrediencí atd).

--- src/components/NewRecipeForm.jsx
+++ src/components/NewRecipeForm.jsx
@@ -2,22 +2,30 @@ import "../css/newRecipeForm.css"
 
 const NewRecipeForm = () => {
     return <form className="newRecipeForm">
-        <a href="#" class="close"></a>
-        <h3>Název receptu:</h3>
-        <input className="recipeName" type="text" />
-        <h3>Ingredience:</h3>
-        <input className="ingredients" type="text" />
-        <h3>Postup:</h3>
-        <input className="instructions" type="text" />
-        <h3>Kategorie</h3>
-        <input className="category" type="text" />
-        <h3>Délka vaření:</h3>
-        <input className="cookTime" type="text" />
-        <h3>Autor:</h3>
-        <input className="autor" type="text" />
-        <h3>Fotka jídla:</h3>
-        <input className="img" type="text" />
-        <button className="submit"></button>
+        <a href="#" className="close"></a>
+        <h3><label htmlFor="recipeName">Název receptu:</label></h3>
+        <input placeholder="např.: Špagety Carbonara" id="recipeName" type="text" />
+        <h3><label htmlFor="ingredients">Ingredience:</label></h3>
+        <input placeholder="Mrkev, Celer, Pažitka..." id="ingredients" type="text" />
+        <h3><label htmlFor="instructions">Postup:</label></h3>
+        <textarea placeholder="např.: Uvařit špagety, smíchat se žloutkem..." id="instructions" type="text" />
+        <h3><label htmlFor="category">Kategorie:</label></h3>
+        <select id="category">
+            <option value="Zvol kategorii">--Zvol kategorii--</option>
+            <option value="Polévky">Polévky</option>
+            <option value="Těstoviny">Těstoviny</option>
+            <option value="Omáčky">Omáčky</option>
+            <option value="Maso">Maso</option>
+            <option value="Bezmasé">Bezmasé</option>
+            <option value="Moučníky">Moučníky</option>
+        </select>
+        <h3><label htmlFor="cookTime">Délka vaření:</label></h3>
+        <input placeholder="min" id="cookTime" type="number" />
+        <h3><label htmlFor="autor">Autor:</label></h3>
+        <input placeholder="Jméno autora, či přezdívka" id="autor" type="text" />
+        <h3><label htmlFor="img">Fotka jídla:</label></h3>
+        <input id="img" type="file" accept="image/jpeg, image/png" />
+        <button className="submit">Přidej recept</button>
     </form>
 }
 
