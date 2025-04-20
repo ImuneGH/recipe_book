b686594ac93408e5b757defaf4bfbfcefe71d219
ImuneGH <MartinSotnar93@gmail.com>
2025-04-20 16:37:25 +0200

    změnil jsem způsob zobrazení Contentu. Výpočty jsem oddělil od jsx, aby se s nimi dalo lépe pracovat a přidal paragraph pro nenalezení žádného receptu.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,13 +3,17 @@ import { motion } from "motion/react"
 import RecipeCard from './RecipeCard'
 
 const Content = ({ activeContent, recipes, activeCategories, searchResult, randomRecipe }) => {
+  const filteredCategory = activeCategories.length && recipes.filter(recipe => activeCategories.includes(recipe.category));
+  const filteredSearch = searchResult !== "" && recipes.filter(recipe => recipe.recipeName.includes(searchResult));
+  const recipesDisplayed = filteredCategory.length ? filteredCategory : filteredSearch.length ? filteredSearch : randomRecipe ? [randomRecipe] : [];
+  const isEmpty = recipesDisplayed.length === 0 && activeContent;
+
   return <main className="content">
               {!activeContent && <motion.h1 layoutId='logo'>
                 <img className='logo' src="/img/flavor_log_logo.png" alt="Logo webu Flavor Log" />
               </motion.h1>}
-              {activeCategories.length !== 0 && recipes.filter(recipe => activeCategories.includes(recipe.category)).map(filteredRecipe => <RecipeCard key={filteredRecipe.ID} filteredRecipe={filteredRecipe} />)}
-              {searchResult !== "" && recipes.filter(recipe => recipe.recipeName.includes(searchResult)).map(filteredRecipe => <RecipeCard key={filteredRecipe.ID} filteredRecipe={filteredRecipe} />)}
-              {randomRecipe && <RecipeCard filteredRecipe={randomRecipe} />}
+              {recipesDisplayed ? recipesDisplayed.map(recipeDisplayed => <RecipeCard key={recipeDisplayed.ID} recipeDisplayed={recipeDisplayed} />) : null}
+              {isEmpty && <p>Obsah je prázdný</p>}
           </main>
 }
 
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,13 +1,13 @@
 import '../css/recipeCard.css'
 
-const RecipeCard = ({ filteredRecipe }) => {
+const RecipeCard = ({ recipeDisplayed }) => {
     return <div className="recipeCard">
-                <img src={filteredRecipe.img} alt="" />
+                <img src={recipeDisplayed.img} alt="" />
                 <div className="recipeInfo">
-                    <p title='Kategorie'>{filteredRecipe.category}</p>
-                    <p title='Doba přípravy'>⏳ {filteredRecipe.cookTime}</p>
+                    <p title='Kategorie'>{recipeDisplayed.category}</p>
+                    <p title='Doba přípravy'>⏳ {recipeDisplayed.cookTime}</p>
                 </div>
-                <h2 className="recipeName">{filteredRecipe.recipeName}</h2>
+                <h2 className="recipeName">{recipeDisplayed.recipeName}</h2>
            </div>;
 }
 
