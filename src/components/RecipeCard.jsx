5a71ea2f95a44d6f9e79d79b0e55996f6467faed
ImuneGH <MartinSotnar93@gmail.com>
2025-08-03 21:17:40 +0200

    smazání animaci, vytvoření nové větve animation

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -15,13 +15,8 @@ const RecipeCard = ({ recipeDisplayed, setActiveCategories, setActiveContent, se
     setRecipeDetailActive(true);
   };
 
-  const card = {
-    start: { opacity: 0, rotateY: -180, x: -100 },
-    end: { opacity: 1, rotateY: 0, x: 0, transition: { duration: 0.3 } },
-  };
-
   return (
-    <motion.div title={recipeDisplayed.recipeName} onClick={showRecipeDetail} className="recipeCard" variants={card} initial="start" animate="end">
+    <motion.div title={recipeDisplayed.recipeName} onClick={showRecipeDetail} className="recipeCard">
       <img src={"backend/" + recipeDisplayed.imgPath} alt="" />
       <div className="recipeInfo">
         <p title="Kategorie">{recipeDisplayed.category}</p>
