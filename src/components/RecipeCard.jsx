f8d61076181385c60bb49982c375f33cf3bf47a0
ImuneGH <MartinSotnar93@gmail.com>
2025-08-03 19:05:29 +0200

    přidal jsem nějakou animaci na zobrazení karet, ale nic moc.. Musím ještě vylepšit...

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,4 +1,5 @@
 import "../css/recipeCard.css";
+import { motion } from "motion/react";
 
 const RecipeCard = ({ recipeDisplayed, setActiveCategories, setActiveContent, setRecipeDetailActive, setSearchResult, setRandomRecipe, setClickedRecipeCard, recipes, setSearchValue }) => {
   const showRecipeDetail = (e) => {
@@ -14,15 +15,20 @@ const RecipeCard = ({ recipeDisplayed, setActiveCategories, setActiveContent, se
     setRecipeDetailActive(true);
   };
 
+  const card = {
+    start: { opacity: 0, rotateY: -180, x: -100 },
+    end: { opacity: 1, rotateY: 0, x: 0, transition: { duration: 0.3 } },
+  };
+
   return (
-    <div title={recipeDisplayed.recipeName} onClick={showRecipeDetail} className="recipeCard">
+    <motion.div title={recipeDisplayed.recipeName} onClick={showRecipeDetail} className="recipeCard" variants={card} initial="start" animate="end">
       <img src={"backend/" + recipeDisplayed.imgPath} alt="" />
       <div className="recipeInfo">
         <p title="Kategorie">{recipeDisplayed.category}</p>
         <p title="Doba přípravy">⏳ {recipeDisplayed.cookTime}</p>
       </div>
       <h2 className="recipeName">{recipeDisplayed.recipeName}</h2>
-    </div>
+    </motion.div>
   );
 };
 
