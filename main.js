9999c71df8adf5424b5f189c5f89537ec5b0bfdb
ImuneGH <MartinSotnar93@gmail.com>
2025-09-04 19:19:25 +0200

    první pokusy o spuštění Electronu

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from Electron;
+import { app, BrowserWindow } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
-import { createServer } from "./backend/server.js";
+// import { createServer } from "./backend/server.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,12 +9,27 @@ const __dirname = path.dirname(__filename);
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-        },
-    });
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname, "dist", "index.html"));
 }
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
